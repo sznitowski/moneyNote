198dafbe90055b600ebf37c4af1844b2416c0d1f
Valentin Sznitowski <78902757+sznitowski@users.noreply.github.com>
2021-08-15 22:54:40 -0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -48,12 +48,6 @@ app.set("views", path.join(__dirname, "views"));
 require("./routes/operation.routes")(app);
 
 
-// user routes
-require('.routes/auth.routes')(app);
-require('.routes/user.routes')(app);
-
-
-
 // Set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
